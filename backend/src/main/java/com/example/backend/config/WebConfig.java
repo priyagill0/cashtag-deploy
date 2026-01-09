716422b89cdf791e3fbe9160bcbf38818f40871e
priyagill0 <2004priyagill@gmail.com>
2026-01-09 01:30:05 -0500

    Enable CORS for Vercel

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://cashtag-deploy.vercel.app/" //  Vercel public domain
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
